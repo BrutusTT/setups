11fdde850f379f352bf09cce2b84a6d85773fc62
brutus <brutusthetschiepel@gmail.com>
2016-07-12 17:27:45 +0100

    commit

--- systems/install.sh
+++ systems/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Environment Setup
 ./base_posix.sh
 
+# OS Detection
 platform='unknown'
 unamestr=`uname`
 
 if [[ "$unamestr" == 'Linux' ]]; then
-	platform='ubuntu'
+	platform='linux'
 
 
 elif [[ "$unamestr" == 'Darwin' ]]; then
@@ -15,6 +17,12 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
 	$platform/brew.sh
 fi
 
+# third party tools
+thirdparty_git.sh
+
+# Yarp & iCub stuff
 $platform/yarp.sh
 $platform/icub_main.sh
+
+# own stuff
 brutustt.sh
---
+++ systems/thirdparty_git.sh
@@ -0,0 +1,32 @@
+####################################################################################################
+# Thirdparty Packages
+####################################################################################################
+#!/bin/bash
+
+# save current directory
+CUR_PWD=`pwd`
+
+# MaryTTS
+if [ ! -d "$SOFTWARE_GIT/marytts-installer" ]; then
+	cd $SOFTWARE_GIT
+	git clone https://github.com/marytts/marytts-installer.git
+fi
+
+cd $SOFTWARE_GIT/marytts-installer
+git pull
+
+
+# Python AR Markers
+if [ ! -d "$SOFTWARE_GIT/python-ar-markers" ]; then
+	cd $SOFTWARE_GIT
+	git clone https://github.com/DebVortex/python-ar-markers.git
+fi
+
+cd $SOFTWARE_GIT/python-ar-markers
+git pull
+python setup.py install
+
+
+
+# go back to the initial current directory
+cd $CUR_PWD
