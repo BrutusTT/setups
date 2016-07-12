4179ed72e67ae3420fb7eee077a6f72d6edd31ee
brutus <brutusthetschiepel@gmail.com>
2016-07-12 15:41:29 +0100

    commit

---
+++ systems/osx/brutustt.sh
@@ -0,0 +1,35 @@
+# save current directory
+CUR_PWD=`pwd`
+
+# SPY Package
+if [ -d "$SOFTWARE_GIT/spy" ]; then
+	cd $SOFTWARE_GIT
+	git clone https://github.com/BrutusTT/spy.git
+fi
+
+cd $SOFTWARE_GIT/spy
+git pull
+python setup.py install
+
+# pyJD Package
+if [ -d "$SOFTWARE_GIT/pyJD" ]; then
+	cd $SOFTWARE_GIT
+	git clone https://github.com/BrutusTT/pyJD.git
+fi
+
+cd $SOFTWARE_GIT/pyJD
+git pull
+python setup.py install
+
+# pyNAO Package
+if [ -d "$SOFTWARE_GIT/pyNAO" ]; then
+	cd $SOFTWARE_GIT
+	git clone https://github.com/BrutusTT/pyNAO.git
+fi
+
+cd $SOFTWARE_GIT/pyNAO
+git pull
+python setup.py install
+
+# go back to the initial current directory
+cd $CUR_PWD
