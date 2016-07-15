5e52460ab84135121aba58c3729bf2cb5ea0073d
brutus <brutusthetschiepel@gmail.com>
2016-07-15 16:17:35 +0100

    refactoring

--- hri_dialogs/modules/state_handler.py
+++ hri_dialogs/modules/state_handler.py
@@ -2,19 +2,16 @@
 #    Copyright (C) 2016 by Ingo Keller                                                             #
 #    <brutusthetschiepel@gmail.com>                                                                #
 ####################################################################################################
-import random
+from thread import start_new_thread
 from spy.modules.BaseModule   import BaseModule, main
-
-import StringIO
-import urllib2
-import urllib
-
-import pyaudio    
 import time
-import wave
 
 import yarp
 import speech_recognition as sr
+
+from dialog   import Dialog
+from database import Database
+from mary_tts import MaryTTS
 
 r = sr.Recognizer()
 m = sr.Microphone()
@@ -30,55 +27,29 @@ class StateHandler(BaseModule):
         self.speechPort  = self.createOutputPort('speech')
         self.pointPort   = self.createOutputPort('point')
         self.textPort    = self.createInputPort('text')
+        self.markerPort  = self.createInputPort('marker', 'buffered')
 
-        # mary settings
-        self.speed       = 1.0
-        self.locale      = 'en_GB'
-        self.voice       = 'dfki-poppy'
-        self.process_url = 'http://127.0.0.1:59125/process'
+        self.connect(self.pointPort.getName(), '/NaoController/rpc')
 
-        # read database
-        with open('Data_base_fruits.csv', 'r') as f:
+        # Load the TTS engine
+        self.tts         = MaryTTS()
 
-            # remove header line
-            # remove windows line ending character
-            data         = [x.replace('\r', '')  for x in f.read().split('\n')[1:]]
+        # Load the object database
+        self.db = Database('Data_base_fruits.csv')
 
-            # split lines into entries and only keep entries that have 3 elements
-            self.db      = [x.split(',') for x in data if len(x.split(',')) == 3]
-
-            # get the set of available colors
-            self.colors  = set([x[2] for x in self.db])
-
-            # get the set of available objects
-            self.objects = set([x[1] for x in self.db])
-
-            # print the loaded database
-            print '--- values ---'
-            print 'colors: ',
-            for x in self.colors:
-                print x, 
-            print ''
-
-            print 'objects:',
-            for x in self.objects:
-                print x, 
-            print ''
-
-            print '--- database ---'
-            for x in self.db:
-                print '%s\t%s\t%s' % tuple(x)
+        # Load the dialog
+        self.dialog = Dialog('complex')
 
         return True
     
     
-    def updateModule(self):
-        
-        bottle = self.textPort.read()
-        if bottle:
-            print bottle.toString()
-        
-        return BaseModule.updateModule(self)
+#     def updateModule(self):
+#         
+#         bottle = self.textPort.read()
+#         if bottle:
+#             print bottle.toString()
+#         
+#         return BaseModule.updateModule(self)
 
 
     def runModule(self, *args):
@@ -119,16 +90,20 @@ class StateHandler(BaseModule):
         bottle  = yarp.Bottle()
         bottle.clear()
 
+        print location
+
         x = 0.1
         y = 0.1
         z = 0.1
 
+        bottle.addString('point')
+        bottle.addString('left')
         bottle.addDouble(x)
         bottle.addDouble(y)
         bottle.addDouble(z)
 
+        
         self.pointPort.write(bottle)
-
 
 
     def recognizeSpeech(self):
@@ -153,12 +128,12 @@ class StateHandler(BaseModule):
                         print("You said {}".format(a))
                         break
             except sr.UnknownValueError:
-                        self.say("Oops! Did not catch that")
+                        self.tts.say("Oops! Did not catch that")
             except sr.RequestError as e:
                         print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
 
-                        # tts.say("Your text is : "+a)#verification 1
-                        # tts.say("SLU will output a semantics for your text.")
+                        # self.tts.say("Your text is : "+a)#verification 1
+                        # self.tts.say("SLU will output a semantics for your text.")
 
         new_sentence = a.split()  # divide a big string in several little string divided by a space (=don't work for composed words). b is a list of little strings.
         print(new_sentence)  # verification 
@@ -167,36 +142,55 @@ class StateHandler(BaseModule):
 
     def findObjects(self):
         
+        self.connect('/HCMarker/markers:o', self.markerPort.getName())
+        
         #
         # Gather Object Locations
         #
-        
-        # TODO: 
-
+        start   = time.time()
+        markers = {}
+        while time.time() - start < 5:
+            
+            bottle = self.markerPort.read()
+            if bottle:
+                count = bottle.get(0).asInt()
+                if count > 0:
+                    # get the lists of markers from the bottle
+                    data_list = bottle.get(1).asList()             
+                    # iterate over all elements in data and retrieve the information
+                    for idx in range(count):
+                        # get the information for one marker
+                        marker_info = data_list.get(idx).asList()          
+                        # information is put into a dictionary
+                        markers[marker_info.get(0).asInt()] = (marker_info.get(1).asInt(), marker_info.get(2).asInt())                
+                
+        yarp.Network.disconnect('/HCMarker/markers:o', self.markerPort.getName())
+            
         # { <object id> : [<x-location>, <y-location>] }
-        foundObjects = {}
+        foundObjects = markers
 
         # { <object name>: [ <color>, [<x-location>, <y-location>] ] }
         self.objects = {}
-        for oid, name, color in self.db:
+        for oid, name, color in self.db.db:
             if oid in foundObjects:
                 self.objects[name] = [color, foundObjects[oid]]
-        
+                print name
+
 
     def start(self):
 
-        tries = 3
-        self.findObjects()
-        
         # MESSAGE DE DEPART
-        self.say("I am listening!")
+        self.say('start')
         
         #///////////////////////////////////////////////////////////////////////////////////////////
         #/                                         MAIN PROGRAM                                    /
         #///////////////////////////////////////////////////////////////////////////////////////////
 
         # Greeting*********************************************
-        self.greetingResponse(self.recognizeSpeech(), tries)
+        self.listenFor(['hello' , 'morning' , 'hi'], success = 'greeting')
+
+        # Explain what is next
+        self.say('explain')
 
         # program stop when stop == True
         stop = False   
@@ -204,102 +198,50 @@ class StateHandler(BaseModule):
 
         # ACTIONS#############################################################################
 
+            self.findObjects()
+
             # Wait for a color in the sentence **************************************
-            color = self.waitForColor()
+            color = None
+            while not color:
+                color = self.listenFor(self.db.colors, fail = 'asr_failed')
+        
+            print "The SLU found a color: %s " % (color)
+            tries = self.db.getObjectCountByColor(color)
 
             for _try in range(tries):
             
                 # Question processing**************************
-                obj   = self.searchColorInDB(color, _try)
-                self.doYouMean(obj)
+                obj   = self.db.searchObjectByColor(color, _try)
+                start_new_thread(self.point, (self.objects[obj], ))
+                self.say('refere_to', obj = obj)
+                time.sleep(2)
     
                 answer = self.recognizeSpeech()
     
                 if 'yes' in answer:
-                    self.say('Yeah!')
+                    self.say('success')
                     break
                             
                 elif 'no' in answer:
-                    self.letMeTryAgain()
+                    self.say('failed') 
 
 
             # after n tries
             if 'no' in answer:
-                self.say("Your are cheating!")
+                self.tts.say("Your are cheating!")
                 time.sleep(0.5)
-                self.say("Which one is it?")
+                self.tts.say("Which one is it?")
                 _ = self.recognizeSpeech()
-                self.say('Ah, I see.')
+                self.tts.say('Ah, I see.')
 
             stop = self.repeat()
                 
         # Leave********************************************
-        self.goodbye()
-
-
-    def greetingResponse(self, phrase, tries):
-
-        # chose response phrase
-        phrases = [ 'Hi!', 
-                    'Hello!',
-                    'Good morning!' ]
-        phrase = phrases[ random.randint(0, len(phrases)-1) ]
-
-        for g in ['hello' , 'morning' , 'hi']:
-            if g in phrase:
-                self.say(phrase)
-                break 
-
-        # Explain what is next
-        self.say('Please play the I spy game with me.')
-        self.say('I will guess %s times.' % tries)
-
-
-    def waitForColor(self):
-        color = None
-        while not color:
-        
-            phrase = self.recognizeSpeech()
-        
-            # COLOR (NAME COMPLEMENT)
-            for c in self.colors:
-                if c in phrase:
-                    color = c
-                    break
-
-            if not color:
-                self.say("Sorry, but I'm only interested in playing the game.")
-    
-        print "The SLU found a color: %s " % (color)
-        return color
-
-
-
-    def doYouMean(self, obj):
-        
-        # chose response phrase
-        phrases = [ 'Is it the %s?', 
-                    'the %s?',
-                    'mmmmm.    That %s?' ]
-        phrase = phrases[ random.randint(0, len(phrases)-1) ]
-
-        # response
-        self.say(phrase % obj)
-#        self.point(self.objects[obj])
-        
-
-    def letMeTryAgain(self):
-        # chose response phrase
-        phrases = [ 'Let me try again.', 
-                    'Oh, but this time' ]
-        phrase = phrases[ random.randint(0, len(phrases)-1) ]
-
-        # response
-        self.say(phrase)
+        self.say('goodbye')
 
 
     def repeat(self):
-        self.say("Do you want to try again?")
+        self.say('try_again')
         while True:
         
             phrase = self.recognizeSpeech()
@@ -310,85 +252,39 @@ class StateHandler(BaseModule):
                 return True
 
             else:
-                self.say("Sorry, but I did not understand you. Please say yes or no.")
+                self.say('asr_failed')
 
 
-    def goodbye(self):
-
-        # chose response phrase
-        phrases = [ 'Thank you for playing the game with me.', 
-                    'Goodbye.',
-                    'Bye bye.' ]
-        phrase = phrases[ random.randint(0, len(phrases)-1) ]
-
-        # response
-        self.say(phrase)
-
-
-    def searchColorInDB(self, color, _try):
-        #Function which receive a color and an iteration and give back an object. E.g. "Which is the 
-        # FIRST RED thing in the list?"
-        #--> "it is the APPLE"
-        
-        # just get all colors in order
-        colors  = [x[2] for x in self.db]
-
-        assert color in colors, 'color unknown'
-        
-        # select the object based on the try
-        indices = [i for i, x in enumerate(colors) if x == color]
-
-        # select the name of the object
-        return self.db[indices[_try]][1]
-
-
-    def say(self, data):
-        
-        """based on http://stackoverflow.com/questions/17657103/how-to-play-wav-file-in-python"""
-        
-        # define stream chunk   
-        chunk = 1024  
-        
-        # open a wav format music  
-        f = wave.open(self.getWaveFile(data))  
-    
-        # instantiate PyAudio  
-        p = pyaudio.PyAudio()  
-        
-        # open stream  
-        stream = p.open(format   = p.get_format_from_width(f.getsampwidth()),  
-                        channels = f.getnchannels(),  
-                        rate     = int(f.getframerate() * self.speed),  
-                        output   = True)  
-        
-        # read data  
-        data = f.readframes(chunk)  
-        
-        # play stream  
-        while data != '':  
-            stream.write(data)  
-            data = f.readframes(chunk)  
-        
-        # stop stream  
-        stream.stop_stream()  
-        stream.close()  
-        
-        # close PyAudio  
-        p.terminate()      
     
 
-    def getWaveFile(self, text):
-        values = { 'INPUT_TYPE' :    'TEXT',
-                   'AUDIO':          'WAVE_FILE',
-                   'OUTPUT_TYPE':    'AUDIO',
-                   'LOCALE':         self.locale,
-                   'INPUT_TEXT':     text,
-                   'VOICE':          self.voice }
-    
-        data        = urllib.urlencode(values)  
-        req         = urllib2.Request(self.process_url, data)
-        response    = urllib2.urlopen(req)
-        return StringIO.StringIO(response.read())
+    def listenFor(self, trigger, success = None, fail = None):
+
+        phrase = self.recognizeSpeech()
+        for x in trigger:
+            if x in phrase:
+                if success:
+                    self.say(success)
+                return x
+
+        if fail:
+            self.say(fail)
+
+
+
+    def say(self, phrase, **kwargs):
+        """ This method fetches a phrase from the dialog and if one was found it will be handed over
+            to the TTS. 
+            
+            Additional named arguments are can be used to fill slots.
+            
+        @param string - phrase as defined by the dialog
+        """
+        speech = self.dialog.response(phrase)
+        if speech:
+            if '%' in speech:
+                speech = speech % (kwargs)
+            self.tts.say(speech)
+
 
 
 if __name__ == '__main__':
