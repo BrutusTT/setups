21d34dc9ff846f594d0a531ed17d5e31138de812
brutus <brutusthetschiepel@gmail.com>
2016-07-18 10:50:34 +0100

    commit

--- systems/base_posix.sh
+++ systems/base_posix.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo "Setup Environment"
+
+if [ ! -d "~/software" ]; then
+	echo "create main software directory"
+	mkdir ~/software
+fi
+
+if [ ! -d "~/software/bin" ]; then
+	echo "create bin directory"
+		mkdir ~/software/bin
+fi
+
+if [ ! -d "~/software/build" ]; then
+	echo "create build directory"
+		mkdir ~/software/build
+fi
+
+if [ ! -d "~/software/git" ]; then
+	echo "create git directory"
+		mkdir ~/software/git
+fi
+
