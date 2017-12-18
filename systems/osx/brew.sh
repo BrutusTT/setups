79b34fa0ffd624c0f5ba3934356f8034380c4309
Ingo Keller <ikeller@tu-dresden.de>
2017-12-18 12:50:58 +0000

    more osx dependencies

--- systems/osx/brew.sh
+++ systems/osx/brew.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+brew tap homebrew/science
+
 brew cask install xquartz
 
-brew install doxygen goocanvas ffmpeg libgnomecanvasmm leveldb lua mc protobuf
+brew install doxygen goocanvas ffmpeg ipopt libgnomecanvasmm leveldb libsvm lua mc protobuf
