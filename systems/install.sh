8e83592ed71fc042a26eedfac0ba5f624de50b91
Ingo Keller <ikeller@tu-dresden.de>
2018-05-09 12:00:25 +0100

    Refactored Environment Handling

--- systems/install.sh
+++ systems/install.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-# OS Detection
-platform='unknown'
-unamestr=`uname`
+# Environment Setup
+source bash_exports
+source bash_aliases
+source $platform/bash_env
 
-if [[ "$unamestr" == 'Linux' ]]; then
-    platform='linux'
+# Install System Packages
 
 
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    platform='osx'
+if [[ "$platform" == 'osx' ]]; then
+    $platform/apt.sh
 
+elif [[ "$platform" == 'linux' ]]; then
     $platform/brew.sh
 fi
 
-# Environment Setup
-source $platform/bash_env
 
 ./base_posix.sh
+
+echo "fetch all GitHub sources"
+
+# go to git dir and remember where we are coming from
+CUR_PWD=`pwd`
+cd $SOFTWARE_GIT
+
+# for all repos in the github_repos.txt
+while read src; do	
+
+	# clone it into the git directory, ignore failure as it might already have been cloned
+	if git clone https://github.com/$src > /dev/null 2>&1; then
+		echo "fetched ${src}"
+	else
+		echo "already exists ${src}"
+	fi
+done < github_repos.txt
+
+# go back where we were coming from
+cd $CUR_PWD
+
+# iterate over all repos and update them
+echo "update all GitHub sources"
+for dir in $SOFTWARE_GIT/*; do 
+	(cd "$dir" && echo "$dir" && git pull);
+done
+
 
 # third party tools
 thirdparty_git.sh
@@ -28,6 +54,8 @@ $platform/yarp.sh
 $platform/icub_main.sh
 $platform/icub_contrib_common.sh
 
-
 # own stuff
 brutustt.sh
+
+
+echo "Don't forget to add the bash_exports and bash_aliases to the .bashrc"
