b3a51efb5da2cf980ff1ceacd32d0a4003158704
Brutus The Tschiepel <brutusthetschiepel@gmail.com>
2018-05-22 14:42:06 +0100

    Update apt.sh

--- systems/linux/apt.sh
+++ systems/linux/apt.sh
@@ -2,4 +2,4 @@ sudo mv /var/cache/app-info/xapian/default /var/cache/app-info/xapian/default_ol
 sudo mv /var/cache/app-info/xapian/default_old /var/cache/app-info/xapian/default
 sudo apt update
 sudo apt upgrade -y
-sudo apt install cmake-curses-gui coinor-libipopt-dev doxygen git graphviz-dev libace-dev libatlas-base-dev libeigen3-dev libgsl-dev libgstreamer0.10-dev libode-dev libopencv-dev libopencv-contrib-dev libqwt-qt5-dev libsdl1.2-dev lua5.3 mc mesa-common-dev parallel portaudio19-dev pyqt5-dev-tools python-pip python-pyaudio qtdeclarative5-dev qtmultimedia5-dev swig  -y
+sudo apt install cmake-curses-gui coinor-libipopt-dev doxygen git graphviz-dev libace-dev libatlas-base-dev libeigen3-dev libgsl-dev libgstreamer0.10-dev libode-dev libopencv-dev libopencv-contrib-dev libqwt-qt5-dev libsdl1.2-dev lua5.3 mc mesa-common-dev parallel portaudio19-dev pyqt5-dev-tools python-pip python-pyaudio qml-module-qtquick* qml-module-qtmultimedia qtdeclarative5-dev qtmultimedia5-dev swig  -y
